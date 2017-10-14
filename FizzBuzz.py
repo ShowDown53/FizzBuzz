2e0cc3c72935ab17836ad2ae543f939b1608ce45
Marko <showdown53@gmail.com>
2017-10-14 03:00:27 +0200

    V1.00

---
+++ FizzBuzz.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+start = "da"
+
+while start.lower() == "da":
+    userInput = raw_input("Vnesite število med 1 in 100: ")
+
+    if not userInput.isdigit():
+        print("Niste vnesli števila.")
+        continue
+
+    userInput = int(userInput)
+
+    if userInput > 100:
+        print("Vnesli ste število večje kot 100.")
+        continue
+
+    if userInput < 1:
+        print("Vnesli ste število manjše kot 1.")
+        continue
+
+    for x in range(1, userInput + 1):
+            if x % 3 == 0:
+                print "fizz"
+            elif x % 5 == 0:
+                print "buzz"
+            elif x % 3 == 0 and x % 5 == 0:
+                print "fizzbuzz"
+            else:
+                print x
+
+    start = raw_input("Želite poskusiti ponovno? Odgovorite z 'Da' ali 'Ne': ")
